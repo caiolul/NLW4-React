f4aac1f73640414b7b79854c26c3ad24731e2f43
caiolul <caiolucena.il@gmail.com>
2021-05-24 22:37:25 -0300

    🐛 FIX: Webpack bug build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 const withPWA = require('next-pwa')
 
 module.exports = withPWA({
-  // future: {webpack5: true},
+  future: {webpack5: true},
   pwa: {
     dest: 'public',
     register: true,
     sw: '/sw.js',
-    skipWaiting: true,
   }
 })
